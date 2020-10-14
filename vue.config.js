f04ea4c496cf39d8d560fedc2c894bae363c5552
jinning <663232104@qq.com>
2020-10-14 16:25:15 +0800

    Fixed

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-      ? './'
-      : './',
+      ? '/'
+      : '/',
     lintOnSave: process.env.NODE_ENV !== 'production',
     devServer: {
         proxy: 'http://localhost:4000'
