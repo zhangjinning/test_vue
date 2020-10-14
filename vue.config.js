912cf9687e1a85d1766c7bb447eaef40b25b17f5
jinning <663232104@qq.com>
2020-10-14 10:25:52 +0800

    Add vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+      ? './'
+      : './',
+    lintOnSave: process.env.NODE_ENV !== 'production',
+    devServer: {
+        proxy: 'http://localhost:4000'
+    }
+}
