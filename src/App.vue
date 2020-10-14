8fe3805e5755da2e1087ec0aea579d651ccc0a3e
jinning <663232104@qq.com>
2020-10-14 18:08:39 +0800

    Decoupling head

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/softcatIndex">Index</router-link>
-  </div>
+  <my-head/>
   <router-view/>
 </template>
+
+<script>
+import Head from './components/Head.vue'
+
+export default {
+  components: {
+    'my-head': Head
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
@@ -16,16 +22,4 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
---
+++ src/components/Head.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/softcatIndex">Index</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'my-head'
+}
+</script>
+
+<style lang="scss">
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
